2dbfa81b908b128967231ce9970f7f274815b718
nakario <nakario.jp@gmail.com>
2017-11-08 14:15:31 +0900

    No regexp

--- isuda.go
+++ isuda.go
@@ -14,7 +14,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 	"net/http/pprof"
@@ -323,16 +322,13 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	}
 	rows.Close()
 
-	keywords := make([]string, 0, 500)
-	for _, entry := range entries {
-		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
-	}
-	re := regexp.MustCompile("("+strings.Join(keywords, "|")+")")
 	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
+	for _, entry := range entries {
+		content = strings.Replace(content, entry.Keyword, func(kw string) string {
+			kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+			return kw2sha[kw]
+		}(entry.Keyword), -1)
+	}
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String()+"/keyword/" + pathURIEscape(kw))
